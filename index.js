b60c9b67559a787fd4a406cf7626b69aa16519a4
Mark <markcfleming1990@gmail.com>
2022-02-28 11:43:03 -0500

    added view queries

--- index.js
+++ index.js
@@ -1,142 +1,180 @@
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const cTablet = require('console.table');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTablet = require("console.table");
 
 //DB connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'goat',
-  database: 'company'
+  host: "localhost",
+  user: "root",
+  password: "goat",
+  database: "company",
 });
 
-connection.connect(err => {
+connection.connect((err) => {
   if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
+  console.log("connected as id " + connection.threadId);
   afterConnection();
 });
 
-
-  afterConnection = () => {
-    console.log("***********************************")
-    console.log("*                                 *")
-    console.log("*        EMPLOYEE MANAGER         *")
-    console.log("*                                 *")
-    console.log("***********************************")
-    promptUser();
-  };
+afterConnection = () => {
+  console.log("***********************************");
+  console.log("*                                 *");
+  console.log("*        EMPLOYEE MANAGER         *");
+  console.log("*                                 *");
+  console.log("***********************************");
+  promptUser();
+};
 
 //inquirer prompt main menu
 const promptUser = () => {
-  inquirer.prompt([
+  inquirer
+    .prompt([
       {
-        name: 'choices',
-        type: 'list',
-        message: 'Please select an option:',
+        name: "choices",
+        type: "list",
+        message: "Please select an option:",
         choices: [
-          'View All Employees',
-          'View All Roles',
-          'View All Departments',
-          'View All Employees By Department',
-          'View Department Budgets',
-          'Update Employee Role',
-          'Update Employee Manager',
-          'Add Employee',
-          'Add Role',
-          'Add Department',
-          'Remove Employee',
-          'Remove Role',
-          'Remove Department',
-          'Exit'
-          ]
-      }
+          "View All Employees",
+          "View All Roles",
+          "View All Departments",
+          "View All Employees By Department",
+          "View Department Budgets",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+          "Remove Employee",
+          "Remove Role",
+          "Remove Department",
+          "Exit",
+        ],
+      },
     ])
     .then((answers) => {
-      const {choices} = answers;
+      const { choices } = answers;
 
-        if (choices === 'View All Employees') {
-            viewAllEmployees();
-        }
-
-        if (choices === 'View All Departments') {
-          viewAllDepartments();
+      if (choices === "View All Employees") {
+        viewAllEmployees();
       }
 
-        if (choices === 'View All Employees By Department') {
-            viewEmployeesByDepartment();
-        }
+      if (choices === "View All Departments") {
+        viewAllDepartments();
+      }
 
-        if (choices === 'Add Employee') {
-            addEmployee();
-        }
+      if (choices === "View All Employees By Department") {
+        viewEmployeesByDepartment();
+      }
 
-        if (choices === 'Remove Employee') {
-            removeEmployee();
-        }
+      if (choices === "Add Employee") {
+        addEmployee();
+      }
 
-        if (choices === 'Update Employee Role') {
-            updateEmployeeRole();
-        }
+      if (choices === "Remove Employee") {
+        removeEmployee();
+      }
 
-        if (choices === 'Update Employee Manager') {
-            updateEmployeeManager();
-        }
+      if (choices === "Update Employee Role") {
+        updateEmployeeRole();
+      }
 
-        if (choices === 'View All Roles') {
-            viewAllRoles();
-        }
+      if (choices === "Update Employee Manager") {
+        updateEmployeeManager();
+      }
 
-        if (choices === 'Add Role') {
-            addRole();
-        }
+      if (choices === "View All Roles") {
+        viewAllRoles();
+      }
 
-        if (choices === 'Remove Role') {
-            removeRole();
-        }
+      if (choices === "Add Role") {
+        addRole();
+      }
 
-        if (choices === 'Add Department') {
-            addDepartment();
-        }
+      if (choices === "Remove Role") {
+        removeRole();
+      }
 
-        if (choices === 'View Department Budgets') {
-            viewDepartmentBudget();
-        }
+      if (choices === "Add Department") {
+        addDepartment();
+      }
 
-        if (choices === 'Remove Department') {
-            removeDepartment();
-        }
+      if (choices === "View Department Budgets") {
+        viewDepartmentBudget();
+      }
 
-        if (choices === 'Exit') {
-            connection.end();
-        }
+      if (choices === "Remove Department") {
+        removeDepartment();
+      }
+
+      if (choices === "Exit") {
+        connection.end();
+      }
+    });
+};
+
+//show all employees
+const viewAllEmployees = () => {
+  let sql = `SELECT employee.id,
+   first_name, 
+   last_name,
+   roles_id,
+   manager_id
+    FROM
+  employee`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`Current Employees`);
+    console.table(response);
+  });
+};
+
+//show all roles
+const viewAllRoles = () => {
+  let sql = `SELECT roles.id,
+   title, 
+   salary,
+   department_id
+    FROM
+  roles`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`Current Employee Roles`);
+    console.table(response);
+  });
+};
+
+//show all departments
+const viewAllDepartments = () => {
+  let sql = `SELECT department.id,
+   department_name
+    FROM
+  department`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`Current Departments`);
+    
+  });
+};
+
+//show employees by Dept
+const viewEmployeesByDepartment = () => {
+  const sql = `SELECT first_name,
+  last_name,
+   department.department_name AS department
+    FROM employee
+    LEFT JOIN roles ON employee.roles_id = roles.id
+    LEFT JOIN department ON roles.department_id = department.id`;
+  connection.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`Current Employees sorted by department`);
+    console.table(response);
+    
   });
 };
 
 
-//show all employees
-const viewAllEmployees = () => {
-  let sql =       `SELECT employee_id, 
-                  first_name, 
-                  last_name, 
-                  role, 
-                  department.department_name AS 'department', 
-                  role.salary
-                  FROM employee, role, department 
-                  WHERE department.id = role.department_id 
-                  AND role.id = employee.role_id
-                  ORDER BY employee.id ASC`;
-connection.query(sql, (error, response) => {
-   if (error) throw error;
-   promptUser();
-});
-};
-
-//show all roles
-
-//show all employees
-
 //add dept
 
-// add rolesloyee  
+// add rolesloyee
 
 promptUser();
